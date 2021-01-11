1871004eb7e86754c9480d5ed1f34cb9563c4f65
zicjin <zicjin@gmail.com>
2021-01-12 00:02:12 +0800

    temp

--- .erb/scripts/ElectronRebuild.js
+++ .erb/scripts/ElectronRebuild.js
@@ -10,8 +10,8 @@ const depKeys = Object.keys(dependencies || {}).filter((k) => k !== 'nodegit')
 console.log('electron-rebuild depKeys:', depKeys)
 
 if (depKeys.length > 0 && fs.existsSync(nodeModulesPath)) {
-  const electronRebuildCmd =
-    '../node_modules/.bin/electron-rebuild --parallel --force --types prod,dev,optional --module-dir .'
+  const onlyModules = depKeys.join(',')
+  const electronRebuildCmd = `../node_modules/.bin/electron-rebuild --parallel --force --only ${onlyModules} --types prod,dev,optional --module-dir .`
   const cmd = process.platform === 'win32' ? electronRebuildCmd.replace(/\//g, '\\') : electronRebuildCmd
   execSync(cmd, {
     cwd: path.join(__dirname, '../../src'),
