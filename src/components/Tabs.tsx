53f09ccb3d533099de840525559f63bb75e94c64
zicjin <zicjin@gmail.com>
2021-01-28 23:25:59 +0800

    temp

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,8 @@
+import React, { useCallback, useEffect, useState, useRef, useReducer } from 'react'
+import css from './Tabs.module.scss'
+
+const Tabs = (): JSX.Element => {
+  return <div className={css.tabs} />
+}
+
+export default Tabs
