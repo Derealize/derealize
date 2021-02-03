20cc1125332499134ce39b7f481fa1c631c1e719
zicjin <zicjin@live.com>
2021-02-03 10:22:10 +0800

    temp

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { useStoreActions, useStoreState } from '../reduxStore'
 import { Project } from '../models/project'
+import useHotkeys from '../utils/useHotkeys'
 import ChromeTabs from '../utils/chrome-tabs'
 import css from './TabBar.module.scss'
 import { ReactComponent as BackgroundSvg } from '../styles/chrome-tabs/background.svg'
@@ -25,16 +26,18 @@ const TabBar = (): JSX.Element => {
 
     chromeTabs.current = new ChromeTabs()
     chromeTabs.current.init(el)
-
-    window.addEventListener('keydown', (event) => {
-      if (event.ctrlKey && event.key === 't') {
-        chromeTabs.current.addTab({
-          title: 'New Tab',
-          favicon: false,
-        })
-      }
-    })
   }, [])
+
+  useHotkeys(
+    'ctrl+t',
+    () => {
+      chromeTabs.current.addTab({
+        title: 'New Tab',
+        favicon: false,
+      })
+    },
+    [],
+  )
 
   return (
     <header className={css.tabbar}>
@@ -104,7 +107,7 @@ const TabBar = (): JSX.Element => {
           tabIndex={0}
         >
           <img
-            srcSet="styles/icons/min-w-10.png 1x, styles/icons/min-w-12.png 1.25x, styles/icons/min-w-15.png 1.5x, styles/icons/min-w-15.png 1.75x, styles/icons/min-w-20.png 2x, styles/icons/min-w-20.png 2.25x, styles/icons/min-w-24.png 2.5x, styles/icons/min-w-30.png 3x, styles/icons/min-w-30.png 3.5x"
+            srcSet="styles/icons/min-k-10.png 1x, styles/icons/min-k-12.png 1.25x, styles/icons/min-k-15.png 1.5x, styles/icons/min-k-15.png 1.75x, styles/icons/min-k-20.png 2x, styles/icons/min-k-20.png 2.25x, styles/icons/min-k-24.png 2.5x, styles/icons/min-k-30.png 3x, styles/icons/min-k-30.png 3.5x"
             draggable="false"
             alt="icon"
           />
@@ -119,7 +122,7 @@ const TabBar = (): JSX.Element => {
           tabIndex={0}
         >
           <img
-            srcSet="styles/icons/max-w-10.png 1x, styles/icons/max-w-12.png 1.25x, styles/icons/max-w-15.png 1.5x, styles/icons/max-w-15.png 1.75x, styles/icons/max-w-20.png 2x, styles/icons/max-w-20.png 2.25x, styles/icons/max-w-24.png 2.5x, styles/icons/max-w-30.png 3x, styles/icons/max-w-30.png 3.5x"
+            srcSet="styles/icons/max-k-10.png 1x, styles/icons/max-k-12.png 1.25x, styles/icons/max-k-15.png 1.5x, styles/icons/max-k-15.png 1.75x, styles/icons/max-k-20.png 2x, styles/icons/max-k-20.png 2.25x, styles/icons/max-k-24.png 2.5x, styles/icons/max-k-30.png 3x, styles/icons/max-k-30.png 3.5x"
             draggable="false"
             alt="icon"
           />
@@ -134,7 +137,7 @@ const TabBar = (): JSX.Element => {
           tabIndex={0}
         >
           <img
-            srcSet="styles/icons/restore-w-10.png 1x, styles/icons/restore-w-12.png 1.25x, styles/icons/restore-w-15.png 1.5x, styles/icons/restore-w-15.png 1.75x, styles/icons/restore-w-20.png 2x, styles/icons/restore-w-20.png 2.25x, styles/icons/restore-w-24.png 2.5x, styles/icons/restore-w-30.png 3x, styles/icons/restore-w-30.png 3.5x"
+            srcSet="styles/icons/restore-k-10.png 1x, styles/icons/restore-k-12.png 1.25x, styles/icons/restore-k-15.png 1.5x, styles/icons/restore-k-15.png 1.75x, styles/icons/restore-k-20.png 2x, styles/icons/restore-k-20.png 2.25x, styles/icons/restore-k-24.png 2.5x, styles/icons/restore-k-30.png 3x, styles/icons/restore-k-30.png 3.5x"
             draggable="false"
             alt="icon"
           />
@@ -149,7 +152,7 @@ const TabBar = (): JSX.Element => {
           tabIndex={0}
         >
           <img
-            srcSet="styles/icons/close-w-10.png 1x, styles/icons/close-w-12.png 1.25x, styles/icons/close-w-15.png 1.5x, styles/icons/close-w-15.png 1.75x, styles/icons/close-w-20.png 2x, styles/icons/close-w-20.png 2.25x, styles/icons/close-w-24.png 2.5x, styles/icons/close-w-30.png 3x, styles/icons/close-w-30.png 3.5x"
+            srcSet="styles/icons/close-k-10.png 1x, styles/icons/close-k-12.png 1.25x, styles/icons/close-k-15.png 1.5x, styles/icons/close-k-15.png 1.75x, styles/icons/close-k-20.png 2x, styles/icons/close-k-20.png 2.25x, styles/icons/close-k-24.png 2.5x, styles/icons/close-k-30.png 3x, styles/icons/close-k-30.png 3.5x"
             draggable="false"
             alt="icon"
           />
