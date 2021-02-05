1baef444a116ef252e3e2aa2624c9a48c69d2f29
zicjin <zicjin@live.com>
2021-02-05 16:59:58 +0800

    temp

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react'
-import { Tabs, TabList, Tab, TabPanels, TabPanel, Grid, Box, GridItem, Button } from '@chakra-ui/react'
+import { Tabs, TabList, Tab, TabPanels, TabPanel, Grid, Box, Stack, Button } from '@chakra-ui/react'
+import dayjs from 'dayjs'
 import cs from 'classnames'
 import { css } from '@emotion/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusCircle, faFolderOpen } from '@fortawesome/free-solid-svg-icons'
 import { useStoreActions, useStoreState } from './reduxStore'
 import { Project } from './models/project'
 import style from './Home.module.scss'
@@ -19,16 +22,25 @@ const Home = (): JSX.Element => {
           <Tab mx={1}>Profile</Tab>
           <Tab mx={1}>Team</Tab>
           <Tab mx={1}>Community</Tab>
+          <Tab mx={1}>Settings</Tab>
         </TabList>
         <TabPanels>
           <TabPanel className={style.projects}>
-            <Grid templateColumns="repeat(5, 1fr)" gap={6} mt={2}>
+            <Stack direction="row" spacing={4}>
+              <Button leftIcon={<FontAwesomeIcon icon={faPlusCircle} />} colorScheme="pink" variant="solid">
+                Add
+              </Button>
+              <Button leftIcon={<FontAwesomeIcon icon={faFolderOpen} />} colorScheme="teal" variant="outline">
+                Open
+              </Button>
+            </Stack>
+            <Grid templateColumns="repeat(5, 1fr)" gap={6} mt={4}>
               {projects.map((p) => (
                 <Box className={style.project} key={p.url} w="xs" boxShadow="md">
                   <div className={style.pattern} />
                   <div className={style.content}>
                     <h4>{p.name}</h4>
-                    <div className={style.infos}>{p.editedTime}</div>
+                    <div className={style.infos}>{dayjs(p.editedTime).fromNow()} edited</div>
                   </div>
                 </Box>
               ))}
@@ -45,6 +57,9 @@ const Home = (): JSX.Element => {
           </TabPanel>
           <TabPanel>
             <p>Community!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Settings!</p>
           </TabPanel>
         </TabPanels>
       </Tabs>
