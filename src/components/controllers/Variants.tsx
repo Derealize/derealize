049541715b23bbde5346cce0f3efaf3de23cb16f
Cheney Jin <zicjin@gmail.com>
2021-03-22 17:53:35 +0800

    temp

--- src/components/controllers/Variants.tsx
+++ src/components/controllers/Variants.tsx
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from 'react'
 import cs from 'classnames'
 import { css } from '@emotion/react'
 import type { Project } from '../../models/project'
-import { ElementStates } from '../../models/controlles'
-import type { ContainerPropertys } from '../../models/controlles/layout'
+import { StateVariants, ListVariants } from '../../models/controlles'
 import { useStoreActions, useStoreState } from '../../reduxStore'
-import style from './Layout.module.scss'
+import style from './Variants.module.scss'
 import type { PreloadWindow } from '../../preload'
 
 declare const window: PreloadWindow
@@ -15,25 +14,88 @@ type Props = {
 }
 
 const Variants: React.FC<Props> = ({ project }: Props): JSX.Element => {
-  const screens = useStoreState<Array<string>>((state) => state.controlles.screens)
-  const selectResponsive = useStoreState<string | null>((state) => state.controlles.selectResponsive)
-  const selectElementState = useStoreState<string | null>((state) => state.controlles.selectElementState)
+  const screenVariants = useStoreState<Array<string>>((state) => state.controlles.screenVariants)
+  const alreadyScreenVariants = useStoreState<Array<string>>((state) => state.controlles.alreadyScreenVariants)
+  const selectScreenVariant = useStoreState<string | null>((state) => state.controlles.selectScreenVariant)
+  const setSelectScreenVariant = useStoreActions((actions) => actions.controlles.setSelectScreenVariant)
+
+  const alreadyStateVariants = useStoreState<Array<string>>((state) => state.controlles.alreadyStateVariants)
+  const selectStateVariant = useStoreState<string | null>((state) => state.controlles.selectStateVariant)
+  const setSelectStateVariant = useStoreActions((actions) => actions.controlles.setSelectStateVariant)
+
+  const alreadyListVariants = useStoreState<Array<string>>((state) => state.controlles.alreadyListVariants)
+  const selectListVariant = useStoreState<string | null>((state) => state.controlles.selectListVariant)
+  const setSelectListVariant = useStoreActions((actions) => actions.controlles.setSelectListVariant)
+
+  const customVariants = useStoreState<Array<string>>((state) => state.controlles.customVariants)
+  const alreadyCustomVariants = useStoreState<Array<string>>((state) => state.controlles.alreadyCustomVariants)
+  const selectCustomVariant = useStoreState<string | null>((state) => state.controlles.selectCustomVariant)
+  const setSelectCustomVariant = useStoreActions((actions) => actions.controlles.setSelectCustomVariant)
 
   return (
     <div className={style.variants}>
-      <div className={style.responsives}>
-        {screens.map((screen) => (
-          <span key={screen} className={cs(style.option, { [style.active]: screen === selectResponsive })}>
+      <div className={style.screenVariants}>
+        {screenVariants.map((screen) => (
+          <span
+            key={screen}
+            className={cs(style.option, {
+              [style.active]: screen === selectScreenVariant,
+              [style.already]: alreadyScreenVariants.includes(screen),
+            })}
+            onClick={() => setSelectScreenVariant(screen)}
+            aria-hidden="true"
+          >
             :{screen}
           </span>
         ))}
-
-        {ElementStates.map((states) => (
-          <span key={states} className={cs(style.option, { [style.active]: states === selectElementState })}>
-            :{states}
+      </div>
+      <div className={style.stateVariants}>
+        {StateVariants.map((state) => (
+          <span
+            key={state}
+            className={cs(style.option, {
+              [style.active]: state === selectStateVariant,
+              [style.already]: alreadyStateVariants.includes(state),
+            })}
+            onClick={() => setSelectStateVariant(state)}
+            aria-hidden="true"
+          >
+            :{state}
           </span>
         ))}
       </div>
+      <div className={style.listVariants}>
+        {ListVariants.map((variant) => (
+          <span
+            key={variant}
+            className={cs(style.option, {
+              [style.active]: variant === selectListVariant,
+              [style.already]: alreadyListVariants.includes(variant),
+            })}
+            onClick={() => setSelectListVariant(variant)}
+            aria-hidden="true"
+          >
+            :{variant}
+          </span>
+        ))}
+      </div>
+      {!!customVariants.length && (
+        <div className={style.customVariants}>
+          {customVariants.map((variant) => (
+            <span
+              key={variant}
+              className={cs(style.option, {
+                [style.active]: variant === selectCustomVariant,
+                [style.already]: alreadyCustomVariants.includes(variant),
+              })}
+              onClick={() => setSelectCustomVariant(variant)}
+              aria-hidden="true"
+            >
+              :{variant}
+            </span>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
--- src/models/controlles/layout.ts
+++ src/models/controlles/layout.ts
@@ -1,26 +1,59 @@
-import { Action, action, Thunk, thunk, actionOn, ActionOn, thunkOn, ThunkOn } from 'easy-peasy'
+import { Action, action, Thunk, thunk, computed, Computed } from 'easy-peasy'
 import clone from 'lodash.clonedeep'
 import type { StoreModel } from '../index'
 import { Broadcast } from '../../backend/backend.interface'
 import type { PreloadWindow } from '../../preload'
-import { ElementStates, VariantsProperty } from '.'
+import { PropertyVariants } from '.'
 
 declare const window: PreloadWindow
 const { send, listen, unlisten } = window.derealize
 
-export type ContainerPropertys = Array<VariantsProperty & { apply: boolean }>
+export type ContainerProperty = PropertyVariants & { apply: boolean }
 
 export interface LayoutModel {
-  container: ContainerPropertys
-  setContainer: Action<LayoutModel, ContainerPropertys>
+  containerPropertys: Array<ContainerProperty>
+  setContainer: Action<LayoutModel, ContainerProperty>
+
+  alreadyScreenVariants: Computed<LayoutModel, Array<string>, StoreModel>
+  alreadyStateVariants: Computed<LayoutModel, Array<string>, StoreModel>
+  alreadyListVariants: Computed<LayoutModel, Array<string>, StoreModel>
+  alreadyCustomVariants: Computed<LayoutModel, Array<string>, StoreModel>
 }
 
 const layoutModel: LayoutModel = {
   container: [{ apply: false }],
-
   setContainer: action((state, payload) => {
-    state.container = payload
+    const property = state.container.find(
+      (prop) => prop.dark === payload.dark && prop.list === payload.list && prop.screen === payload.screen,
+    )
+    if (property) {
+      property.apply = payload.apply
+    } else {
+      state.container.push(payload)
+    }
   }),
+
+  alreadyScreenVariants: computed(
+    [
+      (state, storeState) => storeState.controlles.className,
+      (state, storeState) => storeState.controlles.screenVariants,
+    ],
+    (className, screenVariants) => {
+      if (!className || !screenVariants.length) return []
+      const variants: Array<string> = []
+
+      className.split(' ').forEach((name) => {
+        const words = name.split(':')
+        words.forEach((word, index) => {
+          if (screenVariants.includes(word) && index < words.length - 1) {
+            variants.push(word)
+          }
+        })
+      })
+
+      return variants
+    },
+  ),
 }
 
 export default layoutModel
