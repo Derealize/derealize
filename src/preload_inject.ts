b4f9e1e5a78dac41fe22015bb8915c413b41e404
Elliot <elliotreborn@gmail.com>
2021-04-11 19:10:02 +0800

    temp

--- src/preload_inject.ts
+++ src/preload_inject.ts
@@ -1,13 +1,26 @@
 import { ipcRenderer, contextBridge } from 'electron'
+import 'selector-generator'
 import { connectSocket, send, listen } from './client-ipc'
 import { Handler, Broadcast, ElementPayload } from './backend/backend.interface'
-import getQuerySelector from './utils/getQuerySelector'
 
 let PROJECTID: string | null = null
+let activeElement: HTMLElement | null = null
 
-ipcRenderer.on('setParams', (event: Event, { socketId, projectId }: Record<string, string>) => {
+// eslint-disable-next-line new-cap
+const generator = new (window as any).SelectorGenerator()
+const getSelectorString = (el: Element): string => {
+  return generator.getPath(el).replace('html body ', '').split(' ').join('>')
+}
+
+ipcRenderer.on('setParams', (event: Event, { socketId, projectId, activeSelector }: Record<string, string>) => {
+  console.log('setParams', socketId, projectId, activeSelector)
   PROJECTID = projectId
   connectSocket(socketId)
+
+  if (activeSelector) {
+    activeElement = document.querySelector(activeSelector)
+    activeElement?.setAttribute('data-active', 'true')
+  }
 })
 
 const css = `
@@ -18,9 +31,6 @@ const css = `
     box-shadow: 0 0 0 1px #319795;
   }
 `
-
-let activeElement: HTMLElement | null = null
-let activeQueryString: string
 
 listen(Broadcast.LiveUpdateClass, ({ id, className }: ElementPayload) => {
   if (id === PROJECTID && activeElement) {
@@ -35,19 +45,20 @@ const derealizeListener = async (e: Event) => {
 
   activeElement?.removeAttribute('data-active')
   activeElement = e.target as HTMLElement
-  activeQueryString = getQuerySelector(activeElement)
+  activeElement.setAttribute('data-active', 'true')
+
+  const activeSelector = getSelectorString(activeElement)
+  ipcRenderer.send('setActiveSelector', PROJECTID, activeSelector)
+  console.log('setActiveSelector', activeSelector)
 
   const codePosition = activeElement.getAttribute('data-code')
   if (codePosition) {
     const { tagName, className } = activeElement
     await send(Handler.FocusElement, { id: PROJECTID, codePosition, className, tagName })
-    activeElement.setAttribute('data-active', 'true')
   }
 }
 
-const listenElement = () => {
-  console.log('listenElement')
-
+const listenElement = async () => {
   document.querySelectorAll('[data-code]').forEach((el) => {
     el.removeEventListener('click', derealizeListener)
     el.addEventListener('click', derealizeListener)
@@ -55,25 +66,9 @@ const listenElement = () => {
     el.removeEventListener('contextmenu', derealizeListener)
     el.addEventListener('contextmenu', derealizeListener)
   })
-
-  activeElement = document.querySelector('[data-active]')
-  if (!activeElement && activeQueryString) {
-    console.log('querySelector', activeQueryString)
-    activeElement = document.querySelector(activeQueryString)
-  }
-
-  if (activeElement) {
-    const code = activeElement.getAttribute('data-code')
-    if (code) {
-      const { tagName, className } = activeElement
-      send(Handler.FocusElement, { id: PROJECTID, code, tagName, className })
-    }
-  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  // console.log('injected!', document.body.innerHTML)
-
   const style = document.createElement('style')
   style.appendChild(document.createTextNode(css))
   document.head.appendChild(style)
