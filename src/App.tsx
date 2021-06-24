c18b04c13cd44c4f64a1aebafd50e7ce795f4401
Cheney Jin <zicjin@gmail.com>
2021-06-24 17:50:39 +0800

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React, { useEffect } from 'react'
 import { useStoreActions, useStoreState } from './reduxStore'
-import type { Project } from './models/project.interface'
+import type { Project, ProjectWithRuntime } from './models/project.interface'
 import Home from './Home'
 import TabBar from './components/TabBar'
 import ImportModal from './components/ImportModal'
 import EditProject from './components/Edit'
 import ProjectView from './Project'
 import style from './App.module.scss'
+import type { PreloadWindow } from './preload'
+
+declare const window: PreloadWindow
+const { withRuntime } = window.env
 
 const App = (): JSX.Element => {
   const profileLoadStore = useStoreActions((actions) => actions.profile.loadStore)
-  const projectLoadStore = useStoreActions((actions) => actions.project.loadStore)
+  const projectLoadStore = useStoreActions((actions) =>
+    withRuntime ? actions.projectWithRuntime.loadStore : actions.project.loadStore,
+  )
   const workspaceLoadStore = useStoreActions((actions) => actions.workspace.loadStore)
   const libraryLoadStore = useStoreActions((actions) => actions.library.loadStore)
 
-  const projectListen = useStoreActions((actions) => actions.project.listen)
-  const projectUnListen = useStoreActions((actions) => actions.project.unlisten)
+  const projectListen = useStoreActions((actions) =>
+    withRuntime ? actions.projectWithRuntime.listen : actions.project.listen,
+  )
+  const projectUnListen = useStoreActions((actions) =>
+    withRuntime ? actions.projectWithRuntime.unlisten : actions.project.unlisten,
+  )
 
   const elementListen = useStoreActions((actions) => actions.element.listen)
   const elementUnListen = useStoreActions((actions) => actions.element.unlisten)
 
-  const frontProject = useStoreState<Project | undefined>((state) => state.project.frontProject)
+  const frontProject = useStoreState<Project | ProjectWithRuntime | undefined>((state) =>
+    withRuntime ? state.projectWithRuntime.frontProject : state.project.frontProject,
+  )
 
   useEffect(() => {
     profileLoadStore()
