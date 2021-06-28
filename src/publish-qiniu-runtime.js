d141b8674c201170d0d34c9241552cde8cb39001
Cheney Jin <zicjin@gmail.com>
2021-06-28 11:38:02 +0800

    .gitignore updated

--- src/publish-qiniu-runtime.js
+++ src/publish-qiniu-runtime.js
@@ -18,29 +18,29 @@ const options = { scope: 'derealize' }
 const putPolicy = new qiniu.rs.PutPolicy(options)
 const uploadToken = putPolicy.uploadToken(mac)
 
-const fileName =
+const fileNameWithRuntime =
   process.platform === 'darwin'
-    ? `Derealize-${process.env.npm_package_version}.dmg`
-    : `Derealize-${process.env.npm_package_version}.exe`
-const setupFile = path.join(__dirname, `../release/${fileName}`)
+    ? `Derealize-with-runtime-${process.env.npm_package_version}.dmg`
+    : `Derealize-with-runtime-${process.env.npm_package_version}.exe`
+const setupFileWithRuntime = path.join(__dirname, `../release-with-runtime/${fileNameWithRuntime}`)
 
-formUploader.putFile(uploadToken, fileName, setupFile, putExtra, (respErr, respBody) => {
+formUploader.putFile(uploadToken, fileNameWithRuntime, setupFileWithRuntime, putExtra, (respErr, respBody) => {
   if (respErr) {
-    console.error('putFile', respErr)
+    console.error('putFile with-runtime', respErr)
     return
   }
-  console.log('putFile done.', respBody)
+  console.log('putFile with-runtime done.', respBody)
 
-  const fileNameStatic = process.platform === 'darwin' ? `Derealize.dmg` : `Derealize.exe`
+  const fileNameStatic = process.platform === 'darwin' ? `Derealize-with-runtime.dmg` : `Derealize-with-runtime.exe`
   const optionsStatic = { scope: `derealize:${fileNameStatic}` }
   const putPolicyStatic = new qiniu.rs.PutPolicy(optionsStatic)
   const uploadTokenStatic = putPolicyStatic.uploadToken(mac)
 
-  formUploader.putFile(uploadTokenStatic, fileNameStatic, setupFile, putExtra, (respErr2, respBody2) => {
+  formUploader.putFile(uploadTokenStatic, fileNameStatic, setupFileWithRuntime, putExtra, (respErr2, respBody2) => {
     if (respErr2) {
-      console.error('putFileStatic', respErr2)
+      console.error('putFileStatic with-runtime', respErr2)
       return
     }
-    console.log('putFileStatic done.', respBody2)
+    console.log('putFileStatic with-runtime done.', respBody2)
   })
 })
