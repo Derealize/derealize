db88c8205d26dc87127a33d78f8b4116bb415dc9
Cheney Jin <zicjin@gmail.com>
2021-07-24 11:36:11 +0800

    temp

--- src/components/ImportModal.runtime.tsx
+++ src/components/ImportModal.runtime.tsx
@@ -220,19 +220,27 @@ const ImportProjectWithRuntim = (): JSX.Element => {
             <Grid templateColumns="30% 70%" gap={6}>
               <Box>
                 <FormControl id="url" mt={4} isInvalid={!!errors.url}>
+                  <FormHelperText className="prose">
+                    Before importing your project, please follow{' '}
+                    <a href="https://derealize.com/docs/intro" target="_blank" rel="noreferrer">
+                      our documentation
+                    </a>{' '}
+                    to complete the project configuration.
+                  </FormHelperText>
+
                   <FormLabel htmlFor="url">URL</FormLabel>
                   <Input
                     type="text"
                     {...register('url', { required: true, pattern: gitUrlPattern })}
                     disabled={importloading}
                   />
-                  <FormHelperText className="prose">
+                  {/* <FormHelperText className="prose">
                     If you don&apos;t know what this is, you can read{' '}
                     <a href="https://derealize.com/docs/guides/configuration" target="_blank" rel="noreferrer">
                       our documentation
                     </a>{' '}
                     or ask the front-end engineer of the team for help.
-                  </FormHelperText>
+                  </FormHelperText> */}
                   {errors.url && <FormErrorMessage>This field format is not match</FormErrorMessage>}
                 </FormControl>
 
