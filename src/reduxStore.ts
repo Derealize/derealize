97061077b6edcfd3eb8579af346a949ad3ae5251
Cheney Jin <zicjin@gmail.com>
2021-08-03 16:16:57 +0800

    services/api ignore

--- src/reduxStore.ts
+++ src/reduxStore.ts
@@ -1,27 +1,27 @@
 import { createStore, persist, PersistConfig, createTypedHooks } from 'easy-peasy'
 import model, { StoreModel } from './models'
-import * as apiService from './services/api'
+// import * as apiService from './services/api'
 import type { PreloadWindow } from './preload'
 
 declare const window: PreloadWindow
 
 export const { useStoreActions, useStoreDispatch, useStoreState } = createTypedHooks<StoreModel>()
 
-export interface Injections {
-  apiService: typeof apiService
-}
+// export interface Injections {
+//   apiService: typeof apiService
+// }
 
 const persistConfig: PersistConfig<typeof model> = {
   // blacklist: ['trending'],
 }
 
 const store = createStore(persist(model, persistConfig), {
-  injections: { apiService } as Injections,
+  // injections: { apiService } as Injections,
 })
 
 const debugStore = createStore(model, {
   // 👇 provide injections to our store
-  injections: { apiService } as Injections,
+  // injections: { apiService } as Injections,
 })
 
 // export default window.env.isDev ? debugStore : store
